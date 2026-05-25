3f833a9e2ec3cc317740d6b018fc8d5e142ac9eb
Tharun-cse <69574387+Tharun-cse@users.noreply.github.com>
2026-05-25 17:51:43 -0400

    installed dependencies and install tailwind css

---
+++ Flowsync/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 p-6 text-white text-center">
+      <h1>If this background is bright red, Tailwind is working!</h1>
+    </div>
+  )
+}
+
+export default App
