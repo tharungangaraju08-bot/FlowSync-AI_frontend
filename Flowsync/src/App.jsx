4ee2fa6489e832c909ec5e1e279f94713ab408d0
Tharun-cse <69574387+Tharun-cse@users.noreply.github.com>
2026-05-25 18:16:13 -0400

    created project structure

--- Flowsync/src/App.jsx
+++ Flowsync/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
   return (
     <div className="bg-red-500 p-6 text-white text-center">
-      <h1>If this background is bright red, Tailwind is working!</h1>
+      <h1>If this background is bright red, Tailwind is working! and created project structure</h1>
     </div>
   )
 }
