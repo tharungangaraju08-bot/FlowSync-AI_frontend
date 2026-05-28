52e586cf14ab258881c412a41c94c3d87b403e44
Tharun-cse <69574387+Tharun-cse@users.noreply.github.com>
2026-05-28 10:33:15 -0400

    refactor: add reusable button component

--- Flowsync/src/routes/index.jsx
+++ Flowsync/src/routes/index.jsx
@@ -1,5 +1,6 @@
 //src/routes/index.jsx
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 import Landing from "../pages/LandingPage";
 import Login from "../pages/LoginPage";
 import Signup from "../pages/SignupPage";
@@ -7,17 +8,29 @@ import Dashboard from "../pages/DashboardPage";
 
 const router = createBrowserRouter([
   {
-    path: "/",    element: <Landing />,
-  },
-  { 
-    path: "/login",    element: <Login />,
-  },
-  { 
-    path: "/signup",    element: <Signup />,
+    path: "/",
+    element: (
+      <MainLayout>
+        <Landing />
+      </MainLayout>
+    ),
   },
   {
-    path: "/dashboard",    element: <Dashboard />,
-  }
+    path: "/dashboard",
+    element: (
+      <MainLayout>
+        <Dashboard />
+      </MainLayout>
+    ),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 ]);
 
 export default router;
